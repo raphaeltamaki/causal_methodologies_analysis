05af2314c65e9b0d4d8e72b3af54fb3fc23b6f5a
Raphael Tamaki <raphaeltamaki@fb.com>
2024-04-17 17:54:49 +0200

    Added class ExperimentSetup

--- src/data/experiment_setup.py
+++ src/data/experiment_setup.py
@@ -1,10 +1,49 @@
+from datetime import datetime
 import polars as pl
 import kaggle
 import os
 from pathlib import Path
-from typing import Dict
+from typing import Callable, Dict, List
 
 
-class SetupExperiment:
-    def __init__(self) -> None:
-        pass
+class ExperimentSetup:
+    def __init__(
+            self,
+            date_col: str,
+            target_col: str,
+            treatment_start_date: datetime,
+            treatment_end_date: datetime,
+            treatment_variable: str,
+            treated_groups: List[str]) -> None:
+        
+        """
+        TODO: document
+        """
+        self.date_col = date_col
+        self.target_col = target_col
+        self.treatment_start_date = treatment_start_date
+        self.treatment_end_date = treatment_end_date
+        self.treatment_variable = treatment_variable
+        self.treated_groups = treated_groups
+
+        # On hold paramets
+        self.treated_units = None
+        self.treatment_dates = None
+        self.treatment_effect = None
+
+    def _find_treatment_dates(self, data: pl.DataFrame) -> pl.DataFrame:
+        return (data[self.date_col] >= self.treatment_start_date) & (data[self.date_col] <= self.treatment_end_date)
+
+    def apply_treatment(self, data: pl.DataFrame, treatment_effect_method: Callable) -> pl.DataFrame:
+        """
+        Apply the treatment effect method based on the self.treatment_variable directly on target_col, if
+        the row is within the period defined.
+        """
+        self.treatment_dates = self._find_treatment_dates(data)
+        self.treated_units = (
+            self.treatment_dates &
+            (data[self.treatment_variable].is_in(self.treated_groups))
+            )
+        self.treatment_effect = self.treated_units * data.map_rows(treatment_effect_method)
+        data[self.target_col] = data[self.target_col] * (1 + self.treatment_effect)
+        return data
