e7b3dac428c8ada31ca1398ae77fcd82db48a07c
Raphael Tamaki <raphaeltamaki@fb.com>
2024-05-01 15:10:26 +0200

    added date discretization

--- src/data/data_formatter.py
+++ src/data/data_formatter.py
@@ -1,7 +1,9 @@
 from typing import List
-
 import polars as pl
 
+DAY_DISCRETIZATION = "day"
+MONTH_DISCRETIZATION = "month"
+YEAR_DISCRETIZATION = "year"
 
 class BaseFormater():
 
@@ -12,7 +14,9 @@ class BaseFormater():
             date_col: str,
             target_col: str,
             feature_cols: List[str]=None,
-            date_format: str='%Y-%m-%d') -> None:
+            date_format: str='%Y-%m-%d',
+            date_discretization: str=DAY_DISCRETIZATION
+            ) -> None:
         """
         Inputs
             - id_col: name of the column that, together with date, is unique in the dataset.
@@ -28,10 +32,34 @@ class BaseFormater():
         self.target_col = target_col
         self.feature_cols = [] if feature_cols is None else feature_cols
         self.date_format = date_format
+        self.date_discretization = date_discretization
 
         # Resulting parameters
         self.selected_cols = list(set([self.id_col, self.treatment_col, self.date_col, self.target_col] + self.feature_cols)) # drop duplicates
 
+    @staticmethod
+    def get_year_month(column_name):
+        """
+        Get the the year-date of the date in
+        yyyy-MM format
+        """
+        year = pl.col(column_name).dt.year()
+        month = pl.col(column_name).dt.month()
+        month = pl.when(month < 10).then(pl.concat_str(pl.lit("0"), month)).otherwise(month)
+        return pl.concat_str([year, pl.lit("-"), month]).str.to_datetime("%Y-%m", strict=True).alias(column_name)
+
+    def _discretize_date(self, column_name):
+        """
+        Discretize the date column to yearly, monthly, or daily
+        """
+        if self.date_discretization == YEAR_DISCRETIZATION:
+            yield pl.col(column_name).dt.year().str.to_datetime("%Y", strict=True).alias(column_name)
+        elif self.date_discretization == MONTH_DISCRETIZATION:
+            yield self.get_year_month(column_name).alias(column_name)
+        elif self.date_discretization == DAY_DISCRETIZATION:
+            yield pl.col(column_name).cast(pl.Date).cast(pl.Datetime).alias(column_name)
+        else:
+            raise ValueError(f"Invalid value for discretization, please use either {DAY_DISCRETIZATION}, {MONTH_DISCRETIZATION}, {YEAR_DISCRETIZATION}")
 
     def _format_date_col(self, data: pl.DataFrame) -> pl.DataFrame:
         return (
@@ -39,6 +67,7 @@ class BaseFormater():
             .with_columns(
                 pl.col(self.date_col).str.to_datetime(self.date_format, strict=True)
                 )
+            .with_columns(self._discretize_date(self.date_col))
         )
     
     def _remove_extra_cols(self, data: pl.DataFrame) -> pl.DataFrame:
@@ -84,8 +113,9 @@ class IowaLicorSalesFormatter(BaseFormater):
                  date_col: str="Date",
                  target_col: str="Sale (Dollars)",
                  feature_cols: List[str] = ["Category", "Bottle Volume (ml)"],
-                 date_format: str = '%m/%d/%Y') -> None:
-        super().__init__(id_col, treatment_col, date_col, target_col, feature_cols, date_format)
+                 date_format: str = '%m/%d/%Y',
+                 date_discretization: str=MONTH_DISCRETIZATION) -> None:
+        super().__init__(id_col, treatment_col, date_col, target_col, feature_cols, date_format, date_discretization)
 
     def _transform_target_to_numeric(self, data: pl.DataFrame) -> pl.DataFrame:
         return data.with_columns(
@@ -112,8 +142,9 @@ class SuperstoreSalesFormatter(BaseFormater):
                  date_col: str="Order Date",
                  target_col: str="Sales",
                  feature_cols: List[str] = ["State", "Category", "Sub-Category"],
-                 date_format: str = '%d/%m/%Y') -> None:
-        super().__init__(id_col, treatment_col, date_col, target_col, feature_cols, date_format)
+                 date_format: str = '%d/%m/%Y',
+                 date_discretization: str=MONTH_DISCRETIZATION) -> None:
+        super().__init__(id_col, treatment_col, date_col, target_col, feature_cols, date_format, date_discretization)
 
 
 class LifetimeValueFormatter(BaseFormater):
