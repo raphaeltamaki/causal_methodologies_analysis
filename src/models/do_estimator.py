ee7989131426823e21efa3a4ec7642b6eaddd385
Raphael Tamaki <raphaeltamaki@fb.com>
2024-08-05 19:45:50 +0200

    Adding ATT estimation

--- src/models/do_estimator.py
+++ src/models/do_estimator.py
@@ -14,7 +14,7 @@ class DoWhyEstimator():
         self.model = None
         self.estimate = None
 
-    def fit(self, data: pl.DataFrame) -> None:
+    def fit(self, data: pl.DataFrame, target_units: str='att') -> None:
         pandas_data = self.preprocessing.fit_transform(data).to_pandas()
         self.model= CausalModel(
             data=pandas_data,
@@ -24,9 +24,11 @@ class DoWhyEstimator():
             effect_modifiers=self.preprocessing.X_variables)
         
         identified_estimand = self.model.identify_effect(proceed_when_unidentifiable=False)
-        self.estimate = self.model.estimate_effect(identified_estimand,
-                                 confidence_intervals=True,
-                                 method_name="backdoor.propensity_score_matching")
+        self.estimate = self.model.estimate_effect(
+            identified_estimand,
+            confidence_intervals=True,
+            method_name="backdoor.propensity_score_matching",
+            target_units=target_units)
 
     def predict(self, data: pl.DataFrame) -> pd.Series:
         raise NotImplementedError
@@ -36,8 +38,19 @@ class DoWhyEstimator():
         return self.predict(data)
 
     def estimate_ate(self, data: pl.DataFrame) -> float:
+        self.fit(data, target_units='ate')
         return self.estimate.value * self.preprocessing.get_treated_stats()[1]
 
     def estimate_ate_distribution(self, data: pl.DataFrame, ci: float=0.9) -> Tuple[float]:
+        self.fit(data, target_units='ate')
+        std = self.preprocessing.get_treated_stats()[1]
+        return self.estimate.get_standard_error() * std, self.estimate.get_confidence_intervals(ci)[0] * std, self.estimate.get_confidence_intervals(ci)[1] * std
+    
+    def estimate_att(self, data: pl.DataFrame) -> float:
+        self.fit(data, target_units='att')
+        return self.estimate.value * self.preprocessing.get_treated_stats()[1]
+
+    def estimate_att_distribution(self, data: pl.DataFrame, ci: float=0.9) -> Tuple[float]:
+        self.fit(data, target_units='att')
         std = self.preprocessing.get_treated_stats()[1]
         return self.estimate.get_standard_error() * std, self.estimate.get_confidence_intervals(ci)[0] * std, self.estimate.get_confidence_intervals(ci)[1] * std
