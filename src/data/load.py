e72e023edcf2fc3b5bfae16e3da56699603508db
Raphael Tamaki <raphaeltamaki@fb.com>
2024-10-20 14:59:07 +0200

    fix: removing delta as a format to read from, adding related tests, adapted poetry to install polars depending of Mac or not

--- src/data/load.py
+++ src/data/load.py
@@ -1,21 +1,16 @@
-from dataclasses import dataclass
-import polars as pl
-import kaggle
 import os
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict, Protocol
+
+import kaggle
+import polars as pl
 
 
 class DataPuller(Protocol):
     """Protocol for classes that implement pull_data()"""
     def pull_data(self, data_path: Path) -> None:
         """Pulls data from a remote repository"""
-
-
-class DataLoader(Protocol):
-    """Protocol for classes that implement load_data()"""
-    def load_data(self) -> pl.DataFrame:
-        """Outputs the data"""
 
 @dataclass
 class KaggleDataPuller:
@@ -29,6 +24,12 @@ class KaggleDataPuller:
         kaggle.api.dataset_download_files(
                     self.kaggle_dataset_address, path=data_path, unzip=self.unzip
                 )
+
+class DataLoader(Protocol):
+    """Protocol for classes that implement load_data()"""
+    def load_data(self) -> pl.DataFrame:
+        """Outputs the data"""
+
 @dataclass    
 class LocalDataLoader:
     """Loads data stored locally"""
@@ -39,8 +40,6 @@ class LocalDataLoader:
         """Load dataset from the local file path."""
         if self.data_format == 'csv':
             return pl.read_csv(self.data_path)
-        if self.data_format == 'delta':
-            return pl.read_delta(self.data_path)
         if self.data_format == 'excel':
             return pl.read_excel(self.data_path)
         if self.data_format == 'ipc':
@@ -52,118 +51,118 @@ class LocalDataLoader:
             "{self.data_format} is not a valid data format. The accepted formats are 'csv', 'delta', 'json', and 'ipc'"
             )
 
-class CreateLocalDirectoryIfNotExists:
-    """Mixin to create a directory if it doesn't exists"""
-    def create_path_if_not_exists(self, path: Path) -> None:
-        if not os.path.exists(path):
-            os.makedirs(path)
+# class CreateLocalDirectoryIfNotExists:
+#     """Mixin to create a directory if it doesn't exists"""
+#     def create_path_if_not_exists(self, path: Path) -> None:
+#         if not os.path.exists(path):
+#             os.makedirs(path)
 
-class LocalKaggleDataPullerLoader(KaggleDataPuller, LocalDataLoader):
-    """Downlods a dataset from Kaggle in a local directory, and loads it"""
+# class LocalKaggleDataPullerLoader(KaggleDataPuller, LocalDataLoader):
+#     """Downlods a dataset from Kaggle in a local directory, and loads it"""
 
-class DataLoader2:
-    """
-    This class is responsible for downloading the data that is required for the analysis
-    It is meant to simply download all the relevant datasets in the local enviroment and read it
-    when asked.
-    There are currently 5 datasets being used, all from Kaggle:
-        - Iowa Licor Sales
-        - Wallmart Dataset
-        - Supermarket Sales
-        - Superstore Sales Dataset
-        - Lifetime Value
-        - Novel Corona 2019 Dataset
-    """
+# class DataLoader2:
+#     """
+#     This class is responsible for downloading the data that is required for the analysis
+#     It is meant to simply download all the relevant datasets in the local enviroment and read it
+#     when asked.
+#     There are currently 5 datasets being used, all from Kaggle:
+#         - Iowa Licor Sales
+#         - Wallmart Dataset
+#         - Supermarket Sales
+#         - Superstore Sales Dataset
+#         - Lifetime Value
+#         - Novel Corona 2019 Dataset
+#     """
 
-    def __init__(self, data_path: str = None):
+#     def __init__(self, data_path: str = None):
 
-        self.data_path = (
-            Path(data_path)
-            if data_path is not None
-            else Path(os.path.dirname(os.path.abspath(__file__)))
-        )
-        # Constants
-        self.remote_data_addresses = {
-            "iowa_licor_sales": "residentmario/iowa-liquor-sales",
-            "wallmart_sales": "yasserh/walmart-dataset",
-            "supermarket_sales": "aungpyaeap/supermarket-sales",
-            "superstore_sales": "rohitsahoo/sales-forecasting",
-            "lifetime_value": "baetulo/lifetime-value",
-            "corona": "sudalairajkumar/novel-corona-virus-2019-dataset",
-            "climate_change_earth_temperature": "berkeleyearth/climate-change-earth-surface-temperature-data",
-            "nifty50_stock_market": "rohanrao/nifty50-stock-market-data",
-        }
-        self.raw_data = {}
+#         self.data_path = (
+#             Path(data_path)
+#             if data_path is not None
+#             else Path(os.path.dirname(os.path.abspath(__file__)))
+#         )
+#         # Constants
+#         self.remote_data_addresses = {
+#             "iowa_licor_sales": "residentmario/iowa-liquor-sales",
+#             "wallmart_sales": "yasserh/walmart-dataset",
+#             "supermarket_sales": "aungpyaeap/supermarket-sales",
+#             "superstore_sales": "rohitsahoo/sales-forecasting",
+#             "lifetime_value": "baetulo/lifetime-value",
+#             "corona": "sudalairajkumar/novel-corona-virus-2019-dataset",
+#             "climate_change_earth_temperature": "berkeleyearth/climate-change-earth-surface-temperature-data",
+#             "nifty50_stock_market": "rohanrao/nifty50-stock-market-data",
+#         }
+#         self.raw_data = {}
 
-    def get_data(self, force: bool = False) -> None:
-        """
-        Get the data from the remote repositores and store it in self.data_path
-        If data already exists, just load the data from local repository
-        If force=True, redownload the data and overwrite what exists
-        """
-        kaggle.api.authenticate()
-        for dataset_name, dataset_address in self.remote_data_addresses.items():
-            print(f"Getting {dataset_name} dataset")
+#     def get_data(self, force: bool = False) -> None:
+#         """
+#         Get the data from the remote repositores and store it in self.data_path
+#         If data already exists, just load the data from local repository
+#         If force=True, redownload the data and overwrite what exists
+#         """
+#         kaggle.api.authenticate()
+#         for dataset_name, dataset_address in self.remote_data_addresses.items():
+#             print(f"Getting {dataset_name} dataset")
 
-            download_folder = self.data_path / Path(dataset_name)
-            if not os.path.exists(download_folder):
-                os.makedirs(download_folder)
+#             download_folder = self.data_path / Path(dataset_name)
+#             if not os.path.exists(download_folder):
+#                 os.makedirs(download_folder)
 
-            # download data if we are forcing or if it doesn't exist (folder is empty)
-            if force or not os.listdir(download_folder):
-                print(
-                    f"Downloading {dataset_name} dataset. Storing it in {download_folder}"
-                )
-                kaggle.api.dataset_download_files(
-                    dataset_address, path=download_folder, unzip=True
-                )
-            else:
-                print(f"Dataset {dataset_name} already present\n")
+#             # download data if we are forcing or if it doesn't exist (folder is empty)
+#             if force or not os.listdir(download_folder):
+#                 print(
+#                     f"Downloading {dataset_name} dataset. Storing it in {download_folder}"
+#                 )
+#                 kaggle.api.dataset_download_files(
+#                     dataset_address, path=download_folder, unzip=True
+#                 )
+#             else:
+#                 print(f"Dataset {dataset_name} already present\n")
 
-    @staticmethod
-    def get_file_name(dataset_name) -> Path:
-        """
-        Get the correct file to load, given that some datasets actually consists of multiple files
-        """
-        if dataset_name == "nifty50_stock_market":
-            return Path("NIFTY50_all.csv")
-        elif dataset_name == "climate_change_earth_temperature":
-            return Path("GlobalLandTemperaturesByState.csv")
-        elif dataset_name == "corona":
-            return Path("time_series_covid_19_confirmed.csv")
-        elif dataset_name == "lifetime_value":
-            return Path("test.csv")
-        elif dataset_name == "wallmart_sales":
-            return Path("Walmart.csv")
-        elif dataset_name == "superstore_sales":
-            return Path("train.csv")
-        elif dataset_name == "supermarket_sales":
-            return Path("supermarket_sales - Sheet1.csv")
-        elif dataset_name == "iowa_licor_sales":
-            return Path("Iowa_Liquor_Sales.csv")
+#     @staticmethod
+#     def get_file_name(dataset_name) -> Path:
+#         """
+#         Get the correct file to load, given that some datasets actually consists of multiple files
+#         """
+#         if dataset_name == "nifty50_stock_market":
+#             return Path("NIFTY50_all.csv")
+#         elif dataset_name == "climate_change_earth_temperature":
+#             return Path("GlobalLandTemperaturesByState.csv")
+#         elif dataset_name == "corona":
+#             return Path("time_series_covid_19_confirmed.csv")
+#         elif dataset_name == "lifetime_value":
+#             return Path("test.csv")
+#         elif dataset_name == "wallmart_sales":
+#             return Path("Walmart.csv")
+#         elif dataset_name == "superstore_sales":
+#             return Path("train.csv")
+#         elif dataset_name == "supermarket_sales":
+#             return Path("supermarket_sales - Sheet1.csv")
+#         elif dataset_name == "iowa_licor_sales":
+#             return Path("Iowa_Liquor_Sales.csv")
 
-    def load_dataset(self, dataset_name: str) -> pl.DataFrame:
-        """
-        Loads the dataset of the given name.
-        If dataset doesn't exists, raise error and points to possible inputs
-        Assumes that one file exists in the path. If more than one exists, loads only the first one
-        """
-        if dataset_name not in self.remote_data_addresses:
-            raise ValueError(
-                f"This is not a valid dataset name. Please use one of the following: {', '.join(self.remote_data_addresses.keys())}"
-            )
+#     def load_dataset(self, dataset_name: str) -> pl.DataFrame:
+#         """
+#         Loads the dataset of the given name.
+#         If dataset doesn't exists, raise error and points to possible inputs
+#         Assumes that one file exists in the path. If more than one exists, loads only the first one
+#         """
+#         if dataset_name not in self.remote_data_addresses:
+#             raise ValueError(
+#                 f"This is not a valid dataset name. Please use one of the following: {', '.join(self.remote_data_addresses.keys())}"
+#             )
 
-        # finds all files that are present in the path and get the first one if more than one exists
-        file_path = self.data_path / Path(dataset_name)
-        file_name = self.get_file_name(dataset_name)
-        print(file_name)
-        return pl.read_csv(file_path / file_name)
+#         # finds all files that are present in the path and get the first one if more than one exists
+#         file_path = self.data_path / Path(dataset_name)
+#         file_name = self.get_file_name(dataset_name)
+#         print(file_name)
+#         return pl.read_csv(file_path / file_name)
 
-    def load_all_datasets(self) -> Dict[str, pl.DataFrame]:
-        """
-        Read all datasets and return a dictionary containing the Polars DataFrame of each one of them
-        """
-        output = {}
-        for dataset_name in self.remote_data_addresses:
-            output[dataset_name] = self.load_dataset(dataset_name)
-        return output
+#     def load_all_datasets(self) -> Dict[str, pl.DataFrame]:
+#         """
+#         Read all datasets and return a dictionary containing the Polars DataFrame of each one of them
+#         """
+#         output = {}
+#         for dataset_name in self.remote_data_addresses:
+#             output[dataset_name] = self.load_dataset(dataset_name)
+#         return output
---
+++ src/test/test_data_load.py
@@ -0,0 +1,32 @@
+from ..data.load import KaggleDataPuller, LocalDataLoader
+from ..data.utils import CreateLocalDirectoryIfNotExists
+import polars as pl
+import os
+import shutil
+
+
+def test_kaggle_api():
+    """Tests if methods can create local directories"""
+    local_path = "./pytest_kaggle_api"
+    dataset = 'fedesoriano/heart-failure-prediction' # very small but highly maintained dataset
+    data_name = 'heart.csv'
+    CreateLocalDirectoryIfNotExists().create_path_if_not_exists(local_path)
+    assert len(os.listdir(local_path)) == 0, "Folder is not empty, so test won't properly be able to validate execution"
+    
+    KaggleDataPuller(dataset).pull_data(local_path)
+    assert len(os.listdir(local_path)) > 0, "Error: No data was downloaded"
+    assert data_name in os.listdir(local_path), f"Error: cannot find {data_name}"
+
+    shutil.rmtree(local_path)
+    assert not os.path.exists(local_path), "Error: Temporary folder for testing Kaggle API was not deleted. This will affect future tests"
+
+
+
+def test_local_data_loader():
+    """Test methods to load local data"""
+    
+    data_types = ['csv', 'json', 'ipc', 'excel']
+    data_path = ".data/"
+    for data_type in data_types:
+        assert isinstance(LocalDataLoader(data_path=data_path, data_format=data_type), pl.DataFrame)
+        assert LocalDataLoader(data_path=data_path, data_format=data_type)
